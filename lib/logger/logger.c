74f9bc9aaee3abfcf8a16d68ffa3da41b4162738
ydfdas1f546g1df <niklauskuelling@gmx.ch>
2024-05-27 08:28:33 +0200

    loogin lib

--- lib/logger/logger.c
+++ lib/logger/logger.c
@@ -1,2 +1,39 @@
 #include "logger.h"
-//TODO logger.c
+#include "../telnet/telnet.h"
+static int socket = -1;
+
+void set_socket(int s) {
+    socket = s;
+}
+
+/**
+ *
+ * @param message
+ * @param level
+ */
+void log_message(char *message, log_level level) {
+    if (socket == -1) {
+        return;
+    }
+    char *level_str;
+    switch (level) {
+        case LOG_DEBUG:
+            level_str = "DEBUG";
+            break;
+        case LOG_INFO:
+            level_str = "INFO";
+            break;
+        case LOG_WARNING:
+            level_str = "WARNING";
+            break;
+        case LOG_ERROR:
+            level_str = "ERROR";
+            break;
+        default:
+            level_str = "UNKNOWN";
+            break;
+    }
+    char buffer[256];
+    snprintf(buffer, 256, "[%s]: %s\n", level_str, message);
+    send(socket, buffer, strlen(buffer), 0);
+}
--- lib/logger/logger.h
+++ lib/logger/logger.h
@@ -1,4 +1,14 @@
 #ifndef LEARNESP32_LOGGER_H
 #define LEARNESP32_LOGGER_H
-//TODO logger.h
+
+
+enum log_level {
+    LOG_DEBUG,
+    LOG_INFO,
+    LOG_WARNING,
+    LOG_ERROR
+};
+void set_socket(int socket);
+void log_message(char *message, log_level level);
+
 #endif //LEARNESP32_LOGGER_H
